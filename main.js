88a2bdf844d5ad60fe2bb646507a7d8b738f4cb9
SarimArain99 <sarimarain99@gmail.com>
2024-04-22 12:47:25 +0500

    To Do list

---
+++ main.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let toDos = [];
+let condition = true;
+while (condition) {
+    let listToDo = await inquirer.prompt([
+        {
+            message: "What woult you like ToDo Today",
+            name: "firstQuestion",
+            type: "input",
+        },
+        {
+            name: "secondQuestion",
+            message: "Would you like to add more things",
+            type: "confirm",
+            default: "true"
+        }
+    ]);
+    toDos.push(listToDo.firstQuestion);
+    console.log(toDos);
+    condition = listToDo.secondQuestion;
+}
+;
